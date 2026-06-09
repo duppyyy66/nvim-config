26cb5b742bcd34a88d2484a603f6f092f385f7c1
duppyyy66 <duppyyy66@gmail.com>
2026-06-09 20:58:32 +0300

    added install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+REPO="git@github.com:duppyyy66/nvim-config.git"
+
+echo ">>> Устанавливаем Neovim конфиг..."
+
+# Бэкап старого конфига если есть
+[ -d ~/.config/nvim ] && mv ~/.config/nvim ~/.config/nvim.bak.$(date +%s)
+
+# Клонировать конфиг
+mkdir -p ~/.config
+git clone "$REPO" ~/.config/nvim
+
+# Установить плагины headless (без открытия UI)
+nvim --headless "+Lazy sync" +qa 2>/dev/null
+
+echo ">>> Готово! Запусти nvim"
