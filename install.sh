4d4fd2d4f059ab060a3bc1d0a7337d4324979fbc
duppyyy66 <duppyyy66@gmail.com>
2026-06-09 21:09:49 +0300

    added alias vi

--- install.sh
+++ install.sh
@@ -15,4 +15,13 @@ git clone "$REPO" ~/.config/nvim
 # Установить плагины headless (без открытия UI)
 nvim --headless "+Lazy sync" +qa 2>/dev/null
 
+# Добавить алиас 
+SHELL_RC="$HOME/.bashrc"
+[ -f "$HOME/.zshrc" ] && SHELL_RC="$HOME/.zshrc"
+
+if ! grep -q "alias vi='nvim'" "$SHELL_RC"; then
+  echo "alias vi='nvim'" >> "$SHELL_RC"
+  echo ">>> Добавлен alias vi=nvim в $SHELL_RC"
+fi
+
 echo ">>> Готово! Запусти nvim"
